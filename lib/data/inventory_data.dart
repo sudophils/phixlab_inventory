d279402f654083e5720b471dad77d1a033916702
Philips Ucheolisah Nge <philencripted@gmail.com>
2023-01-05 04:08:40 -0500

    init

---
+++ lib/data/inventory_data.dart
@@ -0,0 +1,13 @@
+import 'models/product_model.dart';
+
+final productInventoryList = [
+  Product(
+      productPhoto: "",
+      productName: "Solid Lapel Neck Blouse",
+      category: ProductCategory.clothing,
+      productSku: "TS38790",
+      productVariants: [ProductVariant.color, ProductVariant.size],
+      productPrice: 24,
+      status: ProductStatus.active,
+      variantCount: 11)
+];
--- lib/data/models/product_model.dart
+++ lib/data/models/product_model.dart
@@ -30,15 +30,3 @@ class Product {
     required this.status,
   });
 }
-
-final productInventoryList = [
-  Product(
-      productPhoto: "",
-      productName: "Solid Lapel Neck Blouse",
-      category: ProductCategory.clothing,
-      productSku: "TS38790",
-      productVariants: [ProductVariant.color, ProductVariant.size],
-      productPrice: 24,
-      status: ProductStatus.active,
-      variantCount: 11)
-];
