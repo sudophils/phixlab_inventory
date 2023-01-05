06ba6ba47f5be2e3af639bad0582fad259807ddf
Philips Ucheolisah Nge <philencripted@gmail.com>
2023-01-05 04:06:25 -0500

    init

---
+++ lib/data/models/product_model.dart
@@ -0,0 +1,44 @@
+enum ProductCategory {
+  clothing,
+  shoes,
+  bag,
+  jewelry,
+}
+
+enum ProductVariant { color, size, material }
+
+enum ProductStatus { active, outOfStock }
+
+class Product {
+  final String productName;
+  final String productPhoto;
+  final ProductCategory category;
+  final String productSku;
+  final int variantCount;
+  final List<ProductVariant> productVariants;
+  final num productPrice;
+  final ProductStatus status;
+
+  Product({
+    required this.variantCount,
+    required this.productPhoto,
+    required this.productName,
+    required this.category,
+    required this.productSku,
+    required this.productVariants,
+    required this.productPrice,
+    required this.status,
+  });
+}
+
+final productInventoryList = [
+  Product(
+      productPhoto: "",
+      productName: "Solid Lapel Neck Blouse",
+      category: ProductCategory.clothing,
+      productSku: "TS38790",
+      productVariants: [ProductVariant.color, ProductVariant.size],
+      productPrice: 24,
+      status: ProductStatus.active,
+      variantCount: 11)
+];
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const defaultSpace = 16.0;
+const Color menuBarColor = Colors.white;
+const Color backgroundColor = Color(0xFFf4f5f7);
+const Color primaryColor = Color(0xFF0845ff);
+const Color defaultIconColor = Color(0xFF83919a);
+const List<Color> productStatusColor = [Color(0xFFcdf5d3), Color(0xFFffd2cc)];
+const Color iconBackdropColor = Color(0xFFe5e9ec);
